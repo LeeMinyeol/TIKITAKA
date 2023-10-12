42ae3be16c4f4c08b9a354cbc878874ac82aa187
LeeMinyeol <ingyeol16@gmail.com>
2023-10-12 12:12:38 +0900

    ADD2

---
+++ Assets/Scripts/Manager/UIManager.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class UIManager : MonoBehaviour
+{
+
+    [SerializeField] private GameObject OptionPanel;
+    [SerializeField] private GameObject RankPanel;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void PopUp()
+    {
+
+    }
+}
